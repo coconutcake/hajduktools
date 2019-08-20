12ffd27b195547143c5ff5cfa96ed400fcd9c565
coconutcake <mateusz.ignatowicz@icloud.com>
2019-08-20 22:02:26 +0200

    verboses added

--- doorscalc/models.py
+++ doorscalc/models.py
@@ -18,20 +18,20 @@ class Door(models.Model):
     
     
 
-    title = models.CharField(max_length=200, blank=False, null=False)
-    code = models.CharField(max_length=3, blank=False, null=False)
-    multiplier = models.DecimalField(max_digits=3, decimal_places=0, blank=False, null=False)
-    discount = models.ForeignKey("Discount", on_delete=models.CASCADE)
-    excluded = models.CharField(max_length=50, default="", help_text="wymiar pomijany: width, height, depth", blank=True, null=False)
-    formula = models.CharField(max_length=200, help_text="example: int(w)*int(h)+2*(int(d)*int(h))", blank=False, null=False)
-    ob_formula = models.CharField(max_length=50, blank=False, null=False)
+    title = models.CharField(verbose_name=u"Nazwa", max_length=200, blank=False, null=False)
+    code = models.CharField(verbose_name=u"Kod", max_length=3, blank=False, null=False)
+    multiplier = models.DecimalField(verbose_name=u"Mnożnik", max_digits=3, decimal_places=0, blank=False, null=False)
+    discount = models.ForeignKey('Discount', verbose_name=u"Zniżka", on_delete=models.CASCADE)
+    excluded = models.CharField(verbose_name=u"Wyłączony wymiar", max_length=50, default="", help_text="wymiar pomijany: width, height, depth", blank=True, null=False)
+    formula = models.CharField(verbose_name=u"Pole powierzchni", max_length=200, help_text="example: int(w)*int(h)+2*(int(d)*int(h))", blank=False, null=False)
+    ob_formula = models.CharField(verbose_name=u"Obwód uszczelki", max_length=50, blank=False, null=False)
 
-    nosna = models.CharField(help_text="Wybierz szybe nośną", choices=szyby, max_length=50, blank=False, null=True)
-    nosna_max = models.FloatField(default=0.48, help_text="Wybierz maksymalną powierzchnie szyby nośnej", max_length=50, blank=False, null=False)
+    nosna = models.CharField(verbose_name=u"Skrzydło nośne", help_text="Wybierz szybe nośną", choices=szyby, max_length=50, blank=False, null=True)
+    nosna_max = models.FloatField(verbose_name=u"Maks. powierzchnia skrzydła nośnego", default=0.48, help_text="Wybierz maksymalną powierzchnie szyby nośnej", max_length=50, blank=False, null=False)
 
-    gasket = models.BooleanField(default=False)
+    gasket = models.BooleanField(verbose_name=u"Uszczelka w zestawie", default=False)
     cover = models.ImageField(upload_to='images/', default="image.png")
-    tooltip = models.CharField(help_text="Wpisz krótki opis pod myszką", default="", blank=False, null=False, max_length=50)
+    tooltip = models.CharField(verbose_name=u"Podpowiedź", help_text="Wpisz krótki opis pod myszką", default="", blank=True, null=False, max_length=50)
     published_date = models.DateTimeField(
             blank=True, null=True)
 
