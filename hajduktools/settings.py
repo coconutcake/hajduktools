65ff3a960208f8a1fa483fa2d76928b0bd29dc2d
coconutcake <mateusz.ignatowicz@icloud.com>
2019-09-11 17:33:55 +0200

    ok

--- hajduktools/settings.py
+++ hajduktools/settings.py
@@ -70,12 +70,12 @@ TEMPLATES = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mp.ignatowicz@gmail.com'
-EMAIL_HOST_PASSWORD = 'lubiete1231886'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'mp.ignatowicz@gmail.com'
+# EMAIL_HOST_PASSWORD = 'lubiete1231886'
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 WSGI_APPLICATION = 'hajduktools.wsgi.application'
 
 
