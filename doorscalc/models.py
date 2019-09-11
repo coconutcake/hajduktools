a192fdf586ee0299141756e612d4f965193ee704
coconutcake <mateusz.ignatowicz@icloud.com>
2019-09-12 00:14:02 +0200

    ok

--- doorscalc/models.py
+++ doorscalc/models.py
@@ -8,28 +8,28 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.core.mail import send_mail, EmailMessage
 
-import smtplib
+# import smtplib
 
-def send_mail(target, topic, msg):
-    subject = topic
-    description =  msg
-    gmail_user =  "mp.ignatowicz@gmail.com" # email id from where you want send mail
-    gmail_pwd ="lubiete1231886"
-    FROM = 'Admin: <from@gmail.com>'
-    TO = target #email id where you want send mail
-    TEXT = description
-    SUBJECT = subject
-    server = smtplib.SMTP_SSL()
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.ehlo()
-    server.starttls()
-    server.login(gmail_user, gmail_pwd)
+# def send_mail(target, topic, msg):
+#     subject = topic
+#     description =  msg
+#     gmail_user =  "mp.ignatowicz@gmail.com" # email id from where you want send mail
+#     gmail_pwd ="lubiete1231886"
+#     FROM = 'Admin: <from@gmail.com>'
+#     TO = target #email id where you want send mail
+#     TEXT = description
+#     SUBJECT = subject
+#     server = smtplib.SMTP_SSL()
+#     server = smtplib.SMTP("smtp.gmail.com", 587)
+#     server.ehlo()
+#     server.starttls()
+#     server.login(gmail_user, gmail_pwd)
 
-    message = """From: %s\nTo: %s\nSubject: %s\n\n%s """ % (FROM, TO, SUBJECT, TEXT)
+#     message = """From: %s\nTo: %s\nSubject: %s\n\n%s """ % (FROM, TO, SUBJECT, TEXT)
 
-    server.sendmail(FROM, TO, message)
-    server.quit() 
-    print('end..............')   
+#     server.sendmail(FROM, TO, message)
+#     server.quit() 
+#     print('end..............')   
 # Create your models here.
 
 
